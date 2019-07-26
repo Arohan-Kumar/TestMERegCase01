2c21ba9905f6d45a9dbf5a00ba04c97837e7bfe9
training_d2.03.07 <training_d2.03.07@DDC2-D-9TLM882.dir.svc.accenture.com>
2019-07-26 08:46:07 +0530

    pls

--- src/main/java/Runner/SendToJenkins.java
+++ src/main/java/Runner/SendToJenkins.java
@@ -1,14 +1,11 @@
 package Runner;
 
 import org.junit.runner.RunWith;
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber .class)
 @CucumberOptions(features="Features",glue="Pack01.LearningCucumber",plugin = {"json:target/cucumber.json"})
-//@CucumberOptions(features="Features",glue="Pack01.LearningCucumber",/*tags= {"@Smoke"},*/
-//plugin={"html:target/cucumber-htmlreport.html"})
 public class SendToJenkins {
 	
 }
