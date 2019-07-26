4cbe01c964ab7efe904b25f369d998e5fc4dcfdb
Arohan-Kumar <53162040+Arohan-Kumar@users.noreply.github.com>
2019-07-26 10:09:37 +0530

    Update SendToJenkins.java

--- src/main/java/Runner/SendToJenkins.java
+++ src/main/java/Runner/SendToJenkins.java
@@ -1,11 +1,10 @@
-// package Runner;
+ package Runner;
 
-// import org.junit.runner.RunWith;
-// import io.cucumber.junit.Cucumber;
-// import io.cucumber.junit.CucumberOptions;
-
-// @RunWith(Cucumber .class)
-// @CucumberOptions(features="Features",glue="Pack01.LearningCucumber",plugin = {"json:target/cucumber.json"})
-// public class SendToJenkins {
+ import org.junit.runner.RunWith;
+ import io.cucumber.junit.Cucumber;
+ import io.cucumber.junit.CucumberOptions;
+ @RunWith(Cucumber .class)
+ @CucumberOptions(features="Features",glue="Pack01.LearningCucumber",plugin = {"json:target/cucumber.json"})
+ public class SendToJenkins {
 	
-// }
+ }
